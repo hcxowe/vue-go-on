be156fa13f28e32d86ba3cfc4d954db6029c4212
hcxowe <hcxowe@126.com>
2022-09-19 16:57:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-mcobc5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-mcobc5)
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <HelloWorld msg="Vite + Vue" />
+    <hr />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<style scoped></style>
