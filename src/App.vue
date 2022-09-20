3dab6c7411c7e6fda911bba889b9d27ef16fd506
hcxowe <hcxowe@126.com>
2022-09-20 16:58:59 +0800

    指令，插件

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,14 @@
     <hr />
     <div>{{ `mouseposition: x: ${x}, y: ${y}` }}</div>
     <hr />
+
+    <input v-focus />
+
+    <hr />
 </template>
 
 <script setup lang="ts">
-import { ref, provide } from 'vue'
+import { ref, provide, mounted } from 'vue'
 import useMouse from './utils/mouse.ts'
 
 import HelloWorld from './components/HelloWorld.vue'
@@ -30,6 +34,10 @@ const privateName = ref('hcxowe')
 provide('privateName', privateName)
 
 const { x, y } = useMouse()
+
+const vFocus1 = {
+    mounted: (el) => el.focus(),
+}
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,33 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import Button from './components/Button.vue'
+import i18nPlugin from './plugins/i18n'
 
 const app = createApp(App)
 
 app.component('Button', Button)
+
+app.directive('focus', {
+    // 在绑定元素的 attribute 前 或事件监听器应用前调用
+    created(el, binding, vnode, prevVnode) {},
+    // 在元素被插入到 DOM 前调用
+    beforeMount(el, binding, vnode, prevVnode) {},
+    // 在绑定元素的父组件及他自己的所有子节点都挂载完成后调用
+    mounted: (el) => el.focus(),
+    // 绑定元素的父组件更新前调用
+    beforeUpdate(el, binding, vnode, prevVnode) {},
+    // 在绑定元素的父组件及他自己的所有子节点都更新后调用
+    updated(el, binding, vnode, prevVnode) {},
+    // 绑定元素的父组件卸载前调用
+    beforeUnmount(el, binding, vnode, prevVnode) {},
+    // 绑定元素的父组件卸载后调用
+    unmounted(el, binding, vnode, prevVnode) {},
+})
+
+app.use(i18nPlugin, {
+    greetings: {
+        hello: 'Bonjour!',
+    },
+})
 
 app.mount('#app')
